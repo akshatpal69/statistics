03949348b66bea93e7067b0ed99572d05484f0da
Akshat <akshatpal18@gmail.com>
2022-03-08 15:41:08 +0530

    batchDeployMinorfixes

--- routes/mmmRouter.js
+++ routes/mmmRouter.js
@@ -5,11 +5,32 @@ const router = express.Router()
 router.post('/', async (req, res) => {
     return res.status(200).json({ message: `homeMMM` })
 })
-router.post('/mean', async (req, res) => {
-    const {principalValues} = req.body;
-    
-    return res.status(200).json({ message: principalValues })
+router.get('/', async (req, res) => {
+    return res.status(200).json({ message: `routes listed here` })
 })
+router.post('/mean', async (req, res) => {
+    //get array from http request body
+    const { terms } = req.body
+    //check if array size is greater than 1
+    if (terms.length > 1) {
+        let sum = 0
+        //add the array values and store them in a variable
+        for (i = 0; i < terms.length; i++) {
+            if (typeof terms[i] == 'number') {
+                sum += terms[i]
+            } else {
+                return res.status(400).json({ message: `${terms[i]} on index ${i} is not a number` })
+            }
+        }
+        // apply the average formula, and store the result in a variable
+        //mean= (sum of terms)/(number of terms)
+        calculatedMean = (sum) / (terms.length)
+        return res.status(200).json({ mean: calculatedMean, message: "ok" })
+    }
+    //response if input received is empty json
+    return res.status(418).json({ message: "not enough values to be calculated" })
+})
+
 router.post('/median', (req, res) => {
     return res.status(200).json({ message: `median` })
 })
